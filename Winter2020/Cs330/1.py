56a430f9f17e21b34f654cc2a235a57daed7bc87
vaibhavsharmakv <vaibhavsharmakv@gmail.com>
2020-01-09 09:21:21 -0600

    9-1-20

---
+++ Winter2020/Cs330/1.py
@@ -0,0 +1,54 @@
+Purpose 1 - Defining the enviorment - user friendly 
+purpose 2 - Resourse allocation
+purpose 3 - Control program - to prevent errors & control I/O
+
+2 imp design issues ?
+	- size and replacement policy 
+
+what is unit of work in system ?
+	- process
+
+kernel vs user mode ?
+	- permissions
+
+why is cache usefull ?
+	- quick acess
+	- speed and less memory acess
+	
+
+2---------------------------------------------
+
+pupose of system call ?
+	- communication betwen user and kernel 
+
+pupose of command interpreter and how diff from kernel interpreter
+	- command interpreter -> system interpreter used as common interpreter 
+	- it is seperated from the kernel interpreter due to permissions 
+	- common interpreter gives access to user for system calls 
+
+5 servcies provided by operating system - memorize this for midterm !!!!!
+	- memory allocation
+	- file system manipulation
+	- and 3 more 
+
+Unix example of system call --- not gonna come in midterm 
+	- process control 
+	- file manipulation - open read write 
+	- information maintaince - get ppid 
+	- couple more 
+
+!!! - tell what sort of call it is based on system call 
+	
+MCQ - command interpreter - modules
+
+
+
+
+
+
+
+
+
+
+
+
